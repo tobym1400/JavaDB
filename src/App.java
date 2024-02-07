04dfe41af9808812fb570d7f9b2b7949dec6867b
tobym1400 <156450576+tobym1400@users.noreply.github.com>
2024-02-07 16:09:11 +0000

    Print results from all tables and individual

--- src/App.java
+++ src/App.java
@@ -5,6 +5,10 @@ public class App {
         for (int i=7; i<12; i++) {
             Db.createTable(i);
         }
+        Db.printAllTables();
+        for (int i=7; i<13; i++) {
+            Db.printTable(i);
+        }
         
     }
 }
--- src/Db.java
+++ src/Db.java
@@ -1,6 +1,7 @@
 import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLSyntaxErrorException;
 
@@ -26,22 +27,59 @@ public class Db {
     public static void createTable(int schoolYear) {
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
         Statement stmt = conn.createStatement();
-    ) {
-        String sql = "USE STUDENTS";
-        stmt.executeUpdate(sql);
-        sql = "CREATE TABLE YEAR"+ schoolYear +" (id INT NOT NULL, name VARCHAR(30), PRIMARY KEY(id) )";
-        stmt.executeUpdate(sql);
-        System.out.println("Table created sucessfully");
-
-    } catch (SQLSyntaxErrorException f) {
-        System.out.println("Table already exists");
-    } catch (SQLException e) {
-        e.printStackTrace();
+        ) {
+            String sql = "USE STUDENTS";
+            stmt.executeUpdate(sql);
+            sql = "CREATE TABLE YEAR"+ schoolYear +" (id INT NOT NULL, name VARCHAR(30), PRIMARY KEY(id) )";
+            stmt.executeUpdate(sql);
+            System.out.println("Table created sucessfully");
+        } catch (SQLSyntaxErrorException f) {
+            System.out.println("Table already exists");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-}
 
-    public static void allResults() {
+    public static void printAllTables() {
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement stmt = conn.createStatement();
+        ) {
+            String sql = "USE STUDENTS";
+            stmt.executeUpdate(sql);
+            ResultSet rs;
+            for (int i=7; i<12; i++) {
+                sql = "SELECT * FROM YEAR" + i;
+                rs = stmt.executeQuery(sql);
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    String name = rs.getString("name");
+                    System.out.printf("%s %s\n",id,name);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void printTable(int schoolYear) {
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement stmt = conn.createStatement();
+        ) {
+            String sql = "USE STUDENTS";
+            stmt.executeUpdate(sql);
+            ResultSet rs;
+            sql = "SELECT * FROM YEAR" + schoolYear;
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                System.out.printf("%s %s\n",id,name);
+            }
+        } catch (SQLSyntaxErrorException f) {
+            System.out.println("Table does not exist");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     
 }
