d1a4833d29180f66a614b9bb8d79657ad9aa932d
tobym1400 <156450576+tobym1400@users.noreply.github.com>
2024-02-12 12:38:38 +0000

    Comments

--- src/Db.java
+++ src/Db.java
@@ -13,10 +13,11 @@ public class Db {
     static final String PASS = "250410270317@Tm";
 
     public static void createDB() {
-        //Open Connection
+        //OPEN CONNECTION
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement stmt = conn.createStatement();
         ) {
+            //CREATE DB
             String sql = "CREATE DATABASE STUDENTS";
             stmt.executeUpdate(sql);
             System.out.println("Database created sucessfully");
@@ -26,11 +27,15 @@ public class Db {
     } 
 
     public static void createTable(int schoolYear) {
+        //OPEN CONNECTION
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
         Statement stmt = conn.createStatement();
         ) {
+            //FIND DB
             String sql = "USE STUDENTS";
             stmt.executeUpdate(sql);
+
+            //CREATE TABLE
             sql = "CREATE TABLE YEAR"+ schoolYear +" (id INT NOT NULL, name VARCHAR(30), PRIMARY KEY(id) )";
             stmt.executeUpdate(sql);
             System.out.println("Table created sucessfully");
@@ -42,11 +47,15 @@ public class Db {
     }
 
     public static void printAllTables() {
+        //OPEN CONNECTION
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
         Statement stmt = conn.createStatement();
         ) {
+            //FIND DB
             String sql = "USE STUDENTS";
             stmt.executeUpdate(sql);
+
+            //RESULTS
             ResultSet rs;
             for (int i=7; i<12; i++) {
                 sql = "SELECT * FROM YEAR" + i;
@@ -63,13 +72,17 @@ public class Db {
     }
 
     public static void printTable(int schoolYear) {
+        //OPEN CONNECTION
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
         Statement stmt = conn.createStatement();
         ) {
+            //SELECT
             String sql = "USE STUDENTS";
             stmt.executeUpdate(sql);
-            ResultSet rs;
             sql = "SELECT * FROM YEAR" + schoolYear;
+            
+            //RESULTS
+            ResultSet rs;
             rs = stmt.executeQuery(sql);
             while (rs.next()) {
                 int id = rs.getInt("id");
@@ -84,28 +97,33 @@ public class Db {
     }
 
     public static void findByID() {
+        //OPEN CONNECTION
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
         Statement stmt = conn.createStatement();
         ) {
+            //INIT
             Scanner scanner = new Scanner(System.in);
 
+            //USER INPUT
             System.out.println("Which year group would you like to search from?");
             int yearGroupQuery = scanner.nextInt();
             System.out.println("Which id would you like to select?");
             int idQuery = scanner.nextInt();
 
+            //SELECT
             String sql = "USE STUDENTS";
             stmt.executeUpdate(sql);
             sql = "SELECT * FROM YEAR" + yearGroupQuery +" WHERE id=" + idQuery;
 
+            //Results
             ResultSet rs = stmt.executeQuery(sql);
-
             while (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 System.out.printf("%s %s\n",id,name);
             }
 
+            //CLOSE
             scanner.close();
         } catch (SQLException e) {
             e.printStackTrace();
